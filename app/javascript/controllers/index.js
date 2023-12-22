29bc6a4dd3f6d86cc34eab1e750b5289a69c8b0c
Thiago <tlcrezende@gmail.com>
2023-12-22 01:19:26 -0300

    Update controller registration

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HeaderController from "./header_controller"
+application.register("header", HeaderController)
